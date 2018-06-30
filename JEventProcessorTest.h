503cd6fe81c97e0aba32a2483958daf08a2caed3
David Lawrence <davidl@jlab.org>
2018-06-30 13:01:24 -0400

    Added some commented out lines.

--- JEventProcessorTest.h
+++ JEventProcessorTest.h
@@ -1,12 +1,17 @@
 
 #include <JANA/JEventProcessor.h>
 
+#include <DAQ/Df125FDCPulse.h>
+
 class JEventProcessorTest: public JEventProcessor
 {
 	public:
 
 	void Process(const std::shared_ptr<const JEvent>& aEvent){
 //		_DBG_<< __FUNCTION__ << _DBG_ENDL_;
+
+//		auto fdcpulses = aEvent->GetT<Df125FDCPulse>();
+//		_DBG_<< " Nfdcpulses = " << fdcpulses.size() << _DBG_ENDL_;
 	}
 
 };
